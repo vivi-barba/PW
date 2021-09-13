f00f8c1ac57fe5660359555a2522daa63c8b49e3
VictoriaBarbosa <68571128+vivi-barba@users.noreply.github.com>
2021-09-13 17:57:24 -0300

    Laço de Repetição

---
+++ num/php/index.php
@@ -0,0 +1,17 @@
+<?php
+$inicial = $_POST['txIni'];
+$fim = $_POST['txFinal'];
+$contador = 0;
+echo "inicial : $inicial <br>";
+echo "fim : $fim <br>";
+
+for( $i = 1; $i <= 10; $i = $i + $fim){
+    
+}
+
+do{
+    echo "$inicial <br>";
+    $inicial ++;
+}while($inicial<= $fim);
+
+?>
